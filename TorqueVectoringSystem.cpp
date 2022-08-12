4df4eb24f18d9866c74a798ad71284a662f8c846
delcoii <email@email.com>
2022-08-12 16:16:13 +0900

    공백추

--- TorqueVectoringSystem.cpp
+++ TorqueVectoringSystem.cpp
@@ -393,7 +393,7 @@ float TorqueVectoringSystem::SumFFandPID(float f_output_throttle, float f_PID_th
 
 float TorqueVectoringSystem::ModifyPedalThrottle(float input, float in_min, float in_max, float out_min, float out_max)
 {
-    return (input - in_min)*(out_max - out_min) /  (in_max - in_min) +out_min;
+    return (input - in_min)*(out_max - out_min) /  (in_max - in_min) + out_min;
 }
 
 
