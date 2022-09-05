a610aec03927a60e71c0715bfbe8402375d98890
unknown <sunghoon8585@naver.com>
2022-09-05 15:16:29 +0900

    mpu 인자 확인

--- etc/I2CModuleTest/i2cmaincode.cpp
+++ etc/I2CModuleTest/i2cmaincode.cpp
@@ -4,8 +4,6 @@
 #include "MPU6050.h"
 
 
-
-
 int main(){
 
     PinName MPU_SDA = p28;
@@ -14,7 +12,7 @@ int main(){
     PinName ADS_SDA = p9;
     PinName ADS_SCL = p10;
 
-    Serial pc(USBTX, USBRX, 115200);
+    Serial pc(USBTX, USBRX, 9600);
 
     I2C I2C_ADS(ADS_SDA, ADS_SCL);
     Adafruit_ADS1115 ads(&I2C_ADS);
@@ -30,14 +28,15 @@ int main(){
     while (1) {
 
         mpu.read(&IMU_gx, &IMU_gy, &IMU_gz, &IMU_ax, &IMU_ay, &IMU_az);
-        pc.printf("yaw rate : %f\t\t", IMU_gx);
+        pc.printf("yaw rate\r\n gx : %f, gy : %f, gz : %f\r\n", IMU_gx, IMU_gy, IMU_gz);
 
         reading = ads.readADC_SingleEnded(0); // read channel 0
         //pc.printf("reading: %d\r\n", reading); // print reading
-        pc.printf("voltage: %f\r\n",reading*0.1875/1000);
+        pc.printf("voltage: %f\r\n\n\n",reading*0.1875/1000);
         //wait(2); // loop 2 sek
         
         //reading = ads.readADC_Differential_2_3(); // differential channel 2-3
         //pc.printf("diff a2, a3: %d\r\n", reading); // print diff
     }
+
 }
