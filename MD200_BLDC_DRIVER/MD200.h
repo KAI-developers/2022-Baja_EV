cabbbca01c194b7545bc69cd0db038ff4695c38a
Seonghoon <kai.develop.acc@gmail.com>
2022-09-22 07:38:45 +0900

    대충 만들어놓음

--- MD200_BLDC_DRIVER/MD200.h
+++ MD200_BLDC_DRIVER/MD200.h
@@ -21,6 +21,9 @@
     to control steering actuator motor(BL9N from MD robot)
 
     using MEGA2560, driving motor in constant speed(internal speed setting in driver)
+    with CHG switch OFF
+
+    수업시간 프로그래밍 갬성으로
 */
 
 #include "Arduino.h"
@@ -31,11 +34,33 @@
 #define EXTERNAL_SPEED      HIGH
 #define INTERNAL_SPEED      LOW
 
+#define CCW                 HIGH
+#define CW                  LOW
+
+#define BRAKE_OFF           HIGH
+#define BRAKE_ON            LOW
+
+#define STOP                HIGH
+#define RUN                 LOW
+
+
 class MD200 {
 private:
-
+    int pin_INT_SPEED_;
+    int pin_DIR_;
+    int pin_START_STOP_;
+    int pin_RUN_BRAKE_;
 
 public:
-    void setINT_SPEED(int pin, int mode);
+    MD200(int pin_INT_SPEED, int pin_DIR, int pin_START_STOP, int pin_RUN_BRAKE);
+
+    void setINT_SPEED(int mode);
+    
+    void enableBrake(int action);
+    void runMotor(int dir, int action);
+
+
+    // 안 쓸 예정
+    void setDIR(int dir);
 };
 
